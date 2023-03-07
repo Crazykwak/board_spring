32e3b92a9566711058fdff538eab321cb94eef36
Crazykwak <kwj1830@naver.com>
2023-03-07 21:55:14 +0900

    feat: HomeContrller 추가 ajax 통신 여부 확인 : 굳

---
+++ src/main/java/CrazyKwak/board/home/HomeController.java
@@ -0,0 +1,15 @@
+package CrazyKwak.board.home;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public ResponseEntity home() {
+        return new ResponseEntity("연결 성공!", HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/CrazyKwak/board/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package CrazyKwak.board.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.cors().configurationSource(corsConfig());
+
+        http.authorizeHttpRequests().anyRequest().permitAll();
+
+        return http.build();
+    }
+
+    private CorsConfigurationSource corsConfig() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("Authorization");
+        configuration.addExposedHeader("Authorization");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
