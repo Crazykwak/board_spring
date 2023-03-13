ea33ea7fa0a474a09ef37b42f71bffb1f0b2ca4c
Crazykwak <kwj1830@naver.com>
2023-03-13 20:31:35 +0900

    fix: MemberController log 삭제

--- src/main/java/CrazyKwak/board/member/controller/MemberController.java
+++ src/main/java/CrazyKwak/board/member/controller/MemberController.java
@@ -3,7 +3,6 @@ package CrazyKwak.board.member.controller;
 import CrazyKwak.board.member.dto.MemberJoinDto;
 import CrazyKwak.board.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,15 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
-@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
 
     @PostMapping("/join")
     public ResponseEntity joinMember(@Validated @RequestBody MemberJoinDto memberJoinDto) {
-
-        log.info(String.valueOf(memberJoinDto));
 
         memberService.joinMember(memberJoinDto);
 
